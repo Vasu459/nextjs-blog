6fe3002a5cbc7a69cfbaeda057f99d38ce792ffc
Vasu Subramanium <subramaniumvasu6@gmail.com>
2024-03-05 20:53:20 +0530

    .

--- .webflowrc.js
+++ .webflowrc.js
@@ -2,6 +2,6 @@ module.exports = {
     host: "https://api.webflow.com",
     rootDir: "./devlink",
     siteId: "65e5c854cfd04f15c7029acc",
-    authToken: "a38237394d7a12bce8b9d5f1dfeda3d323f5bc5f4d05a1c2ad40e1cc224b6d02", 
+    authToken: process.env.WF_AUTH, 
     cssModules: true
   }
